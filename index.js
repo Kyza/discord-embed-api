2507fb160057b3442ad98a335001cd33633d9be5
KyzaGitHub <tylerc2077@gmail.com>
2019-06-10 12:52:58 -0400

    Update

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var http = require('http');
 
 
 http.createServer(function(req, res) {
+  console.log(req);
   res.writeHead(200, {
     'Content-Type': 'text/plain'
   });
-  res.end('Hello HTTP!');
+  res.end("I'm gonna send back an embed with this info:");
 }).listen(process.env.PORT || 5000);
 
 console.log("Server started!");
