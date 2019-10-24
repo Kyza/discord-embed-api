20da65d95127318435f6314d8888054233aa93f7
Kyza <TylerC2077@gmail.com>
2019-10-24 13:05:52 -0400

    Added logging and updated README

--- README.md
+++ README.md
@@ -6,13 +6,30 @@ Hosted with Heroku so I don't have to worry about a lot of stuff such as storing
 
 # Data That's Logged
 
- - What you made.
+ - Embed data.
  - What embed ID that data was saved to.
-
-No IP addresses are logged or saved. All data is anonymous.
+ - Your IP address.
 
 I am not responsable the releasing of any personal information within logged data.
 
 # Why is this data logged?
 
-To keep track of how much the API is used, as well as to help with any errors that may occur.
+## IP
+
+Your IP address is logged to help moderate the API.
+
+I don't allow malicious links such as:
+
+ - IP grabbers.
+ - Malware links.
+ - Anything potentially harmful to any user.
+
+In the case that this happens, the user will be IP banned from this API. You can appeal your ban or report malicious embeds on [my Discord server](https://discord.gg/c9ESSur).
+
+## Embed Data
+
+Embed data is logged to keep track of how much the API is used, as well as to help with any errors that may occur.
+
+# Legal
+
+I am not responsible for any data that passes through my API or any consequences that come with it. Anyone who uses this API surrenders the right for me to handle the data they sent. I do not sell this data to advertisers or profit off of it in any way intentionally. This data is for API moderation purposes to help keep the API ethical.
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var http = require('http');
+var http = require("http");
 var fs = require("fs");
 
 var embeds = {};
 
 function randomString(length) {
-  var result = '';
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -22,106 +23,132 @@ function escapeHtml(unsafe) {
     .replace(/'/g, "&#039;");
 }
 
-http.createServer(function(request, response) {
-  var url = decodeURIComponent(request.url);
-  if (url == "/favicon.ico") {
-    response.writeHead(200, {
-      "Content-Type": "application/octet-stream",
-      "Content-Disposition": "attachment; filename=favicon.ico"
-    });
-    fs.createReadStream("./favicon.ico").pipe(response);
-    return;
-  } else if (url.startsWith("/create")) {
-    console.log("User created an embed: " + url);
+http
+  .createServer(function(request, response) {
+    var url = decodeURIComponent(request.url);
+    if (url == "/favicon.ico") {
+      response.writeHead(200, {
+        "Content-Type": "application/octet-stream",
+        "Content-Disposition": "attachment; filename=favicon.ico"
+      });
+      fs.createReadStream("./favicon.ico").pipe(response);
+      return;
+    } else if (url.startsWith("/create")) {
+      // Getting the IP for moderation purposes.
+      // This is only needed during embed creation.
+      var ip =
+        request.headers["x-forwarded-for"] ||
+        request.connection.remoteAddress ||
+        request.socket.remoteAddress ||
+        (request.connection.socket
+          ? request.connection.socket.remoteAddress
+          : null);
 
-    var requestData = "";
-    request.on("data", function(data) {
-      requestData += data;
-    });
-    request.on("end", function() {
-      if (requestData.trim() != "") {
-        try {
-          var embedID = randomString(10);
+      console.log(ip + " created an embed: " + url);
 
-          embedJSON = JSON.parse(requestData);
+      var requestData = "";
+      request.on("data", function(data) {
+        requestData += data;
+      });
+      request.on("end", function() {
+        if (requestData.trim() != "") {
+          try {
+            var embedID = randomString(10);
 
-          console.log(embedJSON);
-          embeds[embedID] = embedJSON;
+            embedJSON = JSON.parse(requestData);
 
-          embedJSON.id = embedID;
+						console.log(JSON.stringify(embedJSON, null, "\t"));
+						
+            embeds[embedID] = embedJSON;
 
+            embedJSON.id = embedID;
+
+            response.writeHead(200, {
+              "Content-Type": "text/json"
+            });
+            response.end(JSON.stringify(embedJSON));
+            console.log("Saved embed at ID: " + embedID);
+          } catch (e) {
+            response.writeHead(200, {
+              "Content-Type": "text/json"
+            });
+            response.end("Invalid POST JSON.");
+          }
+        } else {
           response.writeHead(200, {
-            'Content-Type': 'text/json'
-          });
-          response.end(JSON.stringify(embedJSON));
-          console.log("Saved embed at ID: " + embedID);
-        } catch (e) {
-          response.writeHead(200, {
-            'Content-Type': 'text/json'
+            "Content-Type": "text/json"
           });
           response.end("Invalid POST JSON.");
         }
-      } else {
-        response.writeHead(200, {
-          'Content-Type': 'text/json'
-        });
-        response.end("Invalid POST JSON.");
-      }
-    });
-
-  } else if (url.startsWith("/embed/")) {
-    var embedID = url.replace("/embed/", "").replace(".json", "");
-    console.log("User requested an embed: " + embedID);
-
-    try {
-      if (!url.endsWith(".json")) {
-        if (embeds[embedID]) {
-          var html = `
-      <title>` + escapeHtml(embeds[embedID].title) + `</title>
-      <meta content="` + escapeHtml(embeds[embedID].description) + `" property="og:description">
-			<meta content="` + escapeHtml(embeds[embedID].image) + `" property="og:image">
-			<link type="application/json+oembed" href="https://discord-embed-api.herokuapp.com/embed/` + embedID + `.json" />
-			<meta content="` + escapeHtml(embeds[embedID].color) + `" name="theme-color">
-      ` + (embeds[embedID].banner ? `<meta name="twitter:card" content="summary_large_image">` : "");
-
-          response.writeHead(200, {
-            'Content-Type': 'text/html'
-          });
-          response.end(html + "This page isn't meant to be viewed by users.");
-        } else {
-          response.writeHead(200, {
-            'Content-Type': 'text/html'
-          });
-          response.end("This page isn't meant to be viewed by users.");
-        }
-      } else {
-        var json = {
-          title: embeds[embedID].title,
-          author_name: embeds[embedID].authorName,
-          author_url: embeds[embedID].authorUrl,
-          provider_name: embeds[embedID].providerName,
-          provider_url: embeds[embedID].providerUrl
-        };
-        response.writeHead(200, {
-          'Content-Type': 'text/json'
-        });
-        response.end(JSON.stringify(json, null, 2));
-      }
-    } catch (e) {
-      console.error(e);
-
-      response.writeHead(200, {
-        'Content-Type': 'text/html'
       });
-      response.end("The embed you requested is invalid or no longer exists.");
+    } else if (url.startsWith("/embed/")) {
+      var embedID = url.replace("/embed/", "").replace(".json", "");
+      console.log("User requested an embed: " + embedID);
+
+      try {
+        if (!url.endsWith(".json")) {
+          if (embeds[embedID]) {
+            var html =
+              `
+      <title>` +
+              escapeHtml(embeds[embedID].title) +
+              `</title>
+      <meta content="` +
+              escapeHtml(embeds[embedID].description) +
+              `" property="og:description">
+			<meta content="` +
+              escapeHtml(embeds[embedID].image) +
+              `" property="og:image">
+			<link type="application/json+oembed" href="https://discord-embed-api.herokuapp.com/embed/` +
+              embedID +
+              `.json" />
+			<meta content="` +
+              escapeHtml(embeds[embedID].color) +
+              `" name="theme-color">
+      ` +
+              (embeds[embedID].banner
+                ? `<meta name="twitter:card" content="summary_large_image">`
+                : "");
+
+            response.writeHead(200, {
+              "Content-Type": "text/html"
+            });
+            response.end(html + "This page isn't meant to be viewed by users.");
+          } else {
+            response.writeHead(200, {
+              "Content-Type": "text/html"
+            });
+            response.end("This page isn't meant to be viewed by users.");
+          }
+        } else {
+          var json = {
+            title: embeds[embedID].title,
+            author_name: embeds[embedID].authorName,
+            author_url: embeds[embedID].authorUrl,
+            provider_name: embeds[embedID].providerName,
+            provider_url: embeds[embedID].providerUrl
+          };
+          response.writeHead(200, {
+            "Content-Type": "text/json"
+          });
+          response.end(JSON.stringify(json, null, 2));
+        }
+      } catch (e) {
+        console.error(e);
+
+        response.writeHead(200, {
+          "Content-Type": "text/html"
+        });
+        response.end("The embed you requested is invalid or no longer exists.");
+      }
+    } else {
+      response.writeHead(200, {
+        "Content-Type": "text/html"
+      });
+      fs.createReadStream("./index.html").pipe(response);
+      return;
     }
-  } else {
-    response.writeHead(200, {
-      "Content-Type": "text/html"
-    });
-    fs.createReadStream("./index.html").pipe(response);
-    return;
-  }
-}).listen(process.env.PORT || 5000);
+  })
+  .listen(process.env.PORT || 5000);
 
 console.log("Server started!");
