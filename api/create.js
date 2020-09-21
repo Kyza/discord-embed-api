ddef42ed5b4deaeb2130bd59d0cf95e8061ba7ba
AA <aa@bigdumb.gq>
2020-09-21 19:47:33 -0400

    brrrrrrrrrrrr

---
+++ api/create.js
@@ -0,0 +1,69 @@
+const faunadb = require('faunadb'),
+  q = faunadb.query,
+  client = new faunadb.Client({ secret: process.env.FAUNA_KEY })
+
+  function randomString(length) {
+    var result = "";
+    var characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+  async function write(obj) {
+    await client.query(
+        q.Create(
+            q.Collection('embedCollection'),
+            { data: obj },
+        )
+        )
+  }
+
+module.exports = async (request, response) => {
+var url = decodeURIComponent(request.url);
+  // Getting the IP for moderation purposes.
+  // This is only needed during embed creation.
+  var ip =
+    request.headers["x-forwarded-for"] ||
+    request.connection.remoteAddress ||
+    request.socket.remoteAddress ||
+    (request.connection.socket
+      ? request.connection.socket.remoteAddress
+      : null);
+
+  console.log(ip + " created an embed: " + url);
+
+  var requestData = "";
+  requestData = request.body;
+    if (requestData.trim() != "") {
+      try {
+        var embedID = randomString(10);
+
+        embedJSON = JSON.parse(requestData);
+
+                    console.log(JSON.stringify(embedJSON, null, "\t"));
+
+        embedJSON.id = embedID;
+
+        await write(embedJSON)
+
+        response.writeHead(200, {
+          "Content-Type": "text/json"
+        });
+        response.end(JSON.stringify(embedJSON));
+        console.log("Saved embed at ID: " + embedID);
+      } catch (e) {
+        response.writeHead(200, {
+          "Content-Type": "text/json"
+        });
+        response.end("Invalid POST JSON." + e);
+      }
+    } else {
+      response.writeHead(200, {
+        "Content-Type": "text/json"});
+      response.end("Invalid POST JSON.");
+    }
+}
---
+++ api/index.js
@@ -0,0 +1,97 @@
+const faunadb = require('faunadb'),
+  q = faunadb.query,
+  client = new faunadb.Client({ secret: process.env.FAUNA_KEY })
+
+  async function get(id) {
+    let settings;
+    try {
+        settings = await client.query(
+            q.Get(
+                q.Match(
+                    q.Index("embedIndex"),
+                    id
+                )
+            )
+        ) 
+        } catch (error) {
+            return false
+        }
+    return settings
+  }
+
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+module.exports = async (request, response) => {
+      const url = request.url
+      var embedID = url.replace('/', '').replace(".json", "");
+      console.log("User requested an embed: " + embedID);
+
+      try {
+        const embedreq = await get(embedID)
+        const embed = embedreq.data
+        console.log(embed)
+        if (!url.endsWith(".json")) {
+          if (embed) {
+            var html =
+              `
+      <title>` +
+              escapeHtml(embed.title) +
+              `</title>
+      <meta content="` +
+              escapeHtml(embed.description) +
+              `" property="og:description">
+			<meta content="` +
+              escapeHtml(embed.image) +
+              `" property="og:image">
+			<link type="application/json+oembed" href="https://em.bigdumb.gq/embed/` +
+              embedID +
+              `.json" />
+			<meta content="` +
+              escapeHtml(embed.color) +
+              `" name="theme-color">
+      ` +
+              (embed.banner
+                ? `<meta name="twitter:card" content="summary_large_image">`
+                : "");
+
+            response.writeHead(200, {
+              "Content-Type": "text/html"
+            });
+            response.end(html + "This page isn't meant to be viewed by users.");
+          } else {
+            response.writeHead(200, {
+              "Content-Type": "text/html"
+            });
+            response.end("This page isn't meant to be viewed by users.");
+          }
+        } else {
+          var json = {
+            title: embed.title,
+            author_name: embed.authorName,
+            author_url: embed.authorUrl,
+            provider_name: embed.providerName,
+            provider_url: embed.providerUrl
+          };
+          response.writeHead(200, {
+            "Content-Type": "text/json"
+          });
+          response.end(JSON.stringify(json, null, 2));
+        }
+      } catch (e) {
+        console.error(e);
+
+        response.writeHead(200, {
+          "Content-Type": "text/html"
+        });
+        response.end("The embed you requested is invalid or no longer exists." + e);
+      }
+    }
+
+console.log("Server started!");
